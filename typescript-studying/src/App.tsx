f3831aecba1eef2b46723e2a564fd81f3ae9d9ad
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-06-01 13:01:33 -0300

    :sparkles: feat:

--- typescript-studying/src/App.tsx
+++ typescript-studying/src/App.tsx
@@ -2,13 +2,16 @@ import "./App.css";
 import MapStudy from "./Studing-structure/Map";
 import ElemetsArray from "./Studing-structure/Elements-Array";
 import Stack from "./Studing-structure/stack";
+import ArrayNumber from "./Array-numbers";
 
 function App() {
   return (
     <>
       <MapStudy />
+      <ArrayNumber />
       <ElemetsArray />
       <Stack />
+      
     </>
   );
 }
---
+++ typescript-studying/src/Array-numbers/index.tsx
@@ -0,0 +1,13 @@
+const ArrayNumber = () => {
+  return (
+    <>
+      <h1>Exemplo de Array</h1>
+      <input type="number" id="numberInput"></input>
+      <button>Adicionar Número</button>
+      <h2>Numeros adicionados:</h2>
+      <div>Resultado</div>
+    </>
+  );
+};
+
+export default ArrayNumber;
